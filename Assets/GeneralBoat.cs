990c84501b8cdb6544d7e5e6344b8efb16fd3358
leondahl <42768870+leondahl@users.noreply.github.com>
2018-10-04 11:08:40 +0200

    Added GeneralBoat.cs

---
+++ Assets/GeneralBoat.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralBoat : MonoBehaviour
+{
+    [Header("General Boat Stats")]
+    public int health;
+    public float firerate;
+    public float cooldown;
+    public float setCooldown;
+    public float spread;
+    public float movementSpeed;
+
+	void Update ()
+    {
+		if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
