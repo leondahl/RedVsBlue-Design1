9204ffc4806567d7e49f8b3e4700d2981c1bad3c
ahallersjo <42804903+ahallersjo@users.noreply.github.com>
2018-10-11 09:32:28 +0200

    La till scener jag jobbat på

---
+++ Scripts/RotatingBot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingBot : MonoBehaviour {
+
+    void Update()
+    {
+        transform.Rotate(0, 0, 25 * Time.deltaTime); //rotates 50 degrees per second around z axis
+    }
+}
