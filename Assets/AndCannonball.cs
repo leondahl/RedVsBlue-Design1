3099995207ae5710f01322396b589bc65ede4646
TechnoloniusGrimclaw <42769649+TechnoloniusGrimclaw@users.noreply.github.com>
2018-10-11 09:06:07 +0200

    Add files via upload

---
+++ Assets/AndCannonball.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndCannonball : MonoBehaviour
+{
+    [Header("Cannonball")]
+    public float firerate = 0.9f;
+    public float cooldown = 1;
+    public float setCooldown = 1;
+    public float spread = 5f;
+    public GameObject cannonballPrefab;
+
+    public Transform point;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        cooldown -= Time.deltaTime;
+
+
+        if (Input.GetButton("Fire1"))
+        { 
+           if (cooldown <= 0)          
+           {
+                Debug.Log("Yeuch");
+                BoomBoom();
+                cooldown = setCooldown;
+           }
+        }
+    }
+    private void BoomBoom()
+    {
+        Instantiate(cannonballPrefab, point.position, point.rotation);
+    }
+}
---
+++ Assets/ShootyCannonball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootyCannonball : MonoBehaviour
+{
+
+    public Rigidbody2D cannonballBody;
+    [Range(0.1f, 1f)]
+    public float speed;
+    [Range(0.1f, 10f)]
+    public float constantSpeed = 10f;
+
+    void Update()
+    {
+        cannonballBody.velocity = constantSpeed * transform.right; 
+    }
+}
