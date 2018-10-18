b7f8fef7a4bd93f94dc9c2eacedf2d2f9a60ccb0
leondahl <42768870+leondahl@users.noreply.github.com>
2018-10-18 09:02:49 +0200

    Buttons

---
+++ Scripts/ButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{	
+    void OnClickMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    void OnClickLevel()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+    void OnClickExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit;
+#endif
+    }
+}
